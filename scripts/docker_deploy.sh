0d5877787db4003966080e436fe5ebec021ad353
Bobby <karma0@gmail.com>
2016-09-09 14:29:54 -0600

    Fixed tail param

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -7,7 +7,7 @@
 docker push 656288215726.dkr.ecr.us-east-1.amazonaws.com/mynameismana-website:latest
 
 # Find the task ID
-task_id="$(aws ecs list-tasks --region us-east-1 --service-name mynameismana-website --cluster MindHive | grep -A2 taskArns | tail -1 | sed 's/"//g' | cut -d'/' -f2 | cut -d',' -f1)"
+task_id="$(aws ecs list-tasks --region us-east-1 --service-name mynameismana-website --cluster MindHive | grep -A1 taskArns | tail -1 | sed 's/"//g' | cut -d'/' -f2 | cut -d',' -f1)"
 
 # Restart AWS
 [ "$task_id" != "" ] && aws ecs stop-task --region us-east-1 --cluster MindHive --task $task_id
