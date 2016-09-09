4b4ab0596cf02c492b7c104775e360af76094bbc
Bobby <karma0@gmail.com>
2016-09-09 14:28:41 -0600

    Fixed deploy script

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -4,10 +4,10 @@
 `aws ecr get-login --region us-east-1`
 
 # Push docker image out
-docker push 656288215726.dkr.ecr.us-east-1.amazonaws.com/larsonagency-website:latest
+docker push 656288215726.dkr.ecr.us-east-1.amazonaws.com/mynameismana-website:latest
 
 # Find the task ID
-task_id="$(aws ecs list-tasks --region us-east-1 --cluster MindHive | grep -A2 taskArns | tail -1 | sed 's/"//g' | cut -d'/' -f2 | cut -d',' -f1)"
+task_id="$(aws ecs list-tasks --region us-east-1 --service-name mynameismana-website --cluster MindHive | grep -A2 taskArns | tail -1 | sed 's/"//g' | cut -d'/' -f2 | cut -d',' -f1)"
 
 # Restart AWS
-aws ecs stop-task --region us-east-1 --cluster MindHive --task $task_id
+[ "$task_id" != "" ] && aws ecs stop-task --region us-east-1 --cluster MindHive --task $task_id
