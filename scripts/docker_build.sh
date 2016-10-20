b3f6ed957091590693d0c505980b7b04f415a1fa
Bobby <karma0@gmail.com>
2016-10-20 14:02:38 -0600

    Changed scripts to a submodule

--- scripts/docker_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t mynameismana-website .
-docker tag mynameismana-website:latest 656288215726.dkr.ecr.us-east-1.amazonaws.com/mynameismana-website:latest
-#docker push 656288215726.dkr.ecr.us-east-1.amazonaws.com/mynameismana-website:latest
-
-exit 0
